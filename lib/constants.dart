50e7dc8bbddfab8624bf2d8b4c148e875f111c55
Yousefjoo17 <91394292+Yousefjoo17@users.noreply.github.com>
2024-03-11 22:57:55 +0200

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,1 @@
-import 'dart:ui';
 
-const List<Color> kColorsList = [
-  Color(0xffD7263D),
-  Color(0xffF46036),
-  Color(0xff2E294E),
-  Color(0xff1B998B),
-  Color(0xffC5D86D),
-  Color(0xffD6D327),
-  Color(0xffF536CF),
-];
