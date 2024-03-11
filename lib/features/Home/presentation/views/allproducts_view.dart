4f021217f76b05a4d8b805ae5e4002706ea165e3
Youssef <yousefyousefyousef335@gmail.com>
2024-03-11 15:29:16 +0200

    trigging all products

--- lib/features/Home/presentation/views/allproducts_view.dart
+++ lib/features/Home/presentation/views/allproducts_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:slash/core/widgets/custom_loading_indicator.dart';
+import 'package:slash/core/widgets/custome_error_widget.dart';
+import 'package:slash/features/Home/presentation/view_models/All_products_cubit/all_products_cubit.dart';
 import 'package:slash/features/Home/presentation/views/widgets/customProductCard.dart';
 
 class AllProductsView extends StatelessWidget {
@@ -24,16 +28,27 @@ class AllProductsView extends StatelessWidget {
               ),
             ),
             const SliverToBoxAdapter(child: SizedBox(height: 16)),
-            SliverGrid(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) => const CustomProductCard(),
-              ),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.65,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 20,
-              ),
+            BlocBuilder<AllProductsCubit, AllProductsState>(
+              builder: (context, state) {
+                if (state is AllProductsSuccess) {
+                  return SliverGrid(
+                    delegate: SliverChildBuilderDelegate((context, index) {
+                      return const CustomProductCard();
+                    }, childCount: state.products.length),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.65,
+                      crossAxisSpacing: 8,
+                      mainAxisSpacing: 20,
+                    ),
+                  );
+                } else if (state is AllProductsFailure) {
+                  return CustomErrorWidget(errMessage: state.errMessage);
+                } else {
+                  return const CustomloadingIndicatorWidget();
+                }
+              },
             )
           ],
         ),
